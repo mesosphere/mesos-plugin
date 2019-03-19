c9a50d3321308cb65aa6fd2164937136402aaea7
Karan Vishwanathan <karanvishwanathan@Mesospheres-MacBook-Pro-4239.local>
2019-03-18 18:16:47 -0700

    construct a dummy task

--- src/main/java/org/jenkinsci/plugins/mesos/MesosApi.java
+++ src/main/java/org/jenkinsci/plugins/mesos/MesosApi.java
@@ -2,18 +2,31 @@ package org.jenkinsci.plugins.mesos;
 
 import akka.actor.ActorSystem;
 import akka.stream.ActorMaterializer;
+import akka.stream.OverflowStrategy;
 import akka.stream.javadsl.Sink;
 import com.mesosphere.mesos.client.MesosClient;
 import com.mesosphere.mesos.client.MesosClient$;
 import com.mesosphere.mesos.conf.MesosClientSettings;
+import com.mesosphere.usi.core.models.*;
+import com.mesosphere.usi.core.models.resources.ResourceRequirement;
+import com.mesosphere.usi.core.models.resources.ScalarRequirement;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigValueFactory;
+import com.mesosphere.usi.core.javaapi.SchedulerAdapter;
+import com.mesosphere.usi.core.Scheduler;
+import scala.collection.JavaConverters;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.mesos.v1.Protos;
+import scala.collection.Seq;
+import scala.concurrent.ExecutionContext;
 
 public class MesosApi {
 
@@ -22,9 +35,11 @@ public class MesosApi {
   private final Protos.FrameworkID frameworkId;
   private final MesosClientSettings clientSettings;
   private final MesosClient client;
+  private final SchedulerAdapter adapter;
 
   private final ActorSystem system;
   private final ActorMaterializer materializer;
+  private final ExecutionContext context;
 
   /**
    * Establishes a connection to Mesos and provides a simple interface to start and stop {@link
@@ -49,9 +64,11 @@ public class MesosApi {
             .withValue("master-url", ConfigValueFactory.fromAnyRef(masterUrl));
     this.clientSettings = MesosClientSettings.fromConfig(conf);
     system = ActorSystem.create("mesos-scheduler");
+    context = system.dispatcher();
     materializer = ActorMaterializer.create(system);
-
     client = connectClient().get();
+
+    adapter = new SchedulerAdapter(Scheduler.fromClient(client), materializer, context);
   }
 
   /**
@@ -60,6 +77,10 @@ public class MesosApi {
    * @return A future reference to the {@link MesosSlave}.
    */
   public CompletableFuture<MesosSlave> startAgent() {
+    PodSpec spec = buildMesosAgentTask(0.1, 32);
+    SpecsSnapshot snapshot = new SpecsSnapshot(convertListToSeq(Arrays.asList(spec)), null);
+    adapter.asAkkaQueues(snapshot, OverflowStrategy.backpressure()).t1();
+
     throw new NotImplementedException();
   }
 
@@ -81,5 +102,23 @@ public class MesosApi {
         .apply(clientSettings, frameworkInfo, system, materializer)
         .runWith(Sink.head(), materializer)
         .toCompletableFuture();
+  }
+
+  private PodSpec buildMesosAgentTask(double cpu, double mem) {
+    RunSpec spec = new RunSpec(
+        convertListToSeq(Arrays.asList(ScalarRequirement.cpus(cpu), ScalarRequirement.memory(mem))),
+        "echo Hello!",
+        convertListToSeq(Collections.emptyList())
+    );
+    PodSpec podSpec = new PodSpec(
+        new PodId("jenkins-test"),
+        new Goal.Running$(),
+        spec
+    );
+    return podSpec;
+  }
+
+  private <T> Seq<T> convertListToSeq(List<T> inputList) {
+    return JavaConverters.asScalaIteratorConverter(inputList.iterator()).asScala().toSeq();
   }
 }
